dfe177dfd5f5f0d2a08957d9226059f91d103fc3
GinoGiordano <54674820+GinoGiordano@users.noreply.github.com>
2023-03-11 20:47:01 -0300

    pwa

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import BootstrapVue from 'bootstrap-vue'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './registerServiceWorker'
 Vue.use(BootstrapVue)
 
 new Vue({
