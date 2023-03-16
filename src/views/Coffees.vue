f4fb937ee91e0e07ba90a24e3491a87cf8a573e9
GinoGiordano <54674820+GinoGiordano@users.noreply.github.com>
2023-03-15 23:53:20 -0300

    Update Coffees.vue

--- src/views/Coffees.vue
+++ src/views/Coffees.vue
@@ -6,6 +6,13 @@
                 <CoffeeCard :coffee="coffee"/>
             </b-col>
         </b-container>
+
+        <b-container>
+    <h1>CoffeeBahia</h1>
+    <h2>Publicaciones relacionadas al mundo del Cafe, de Etiopia a Starbucks,la bebida mas amada y mas odiada del mundo.</h2>
+    <h3>Todo lo relacionado al mundo del cafe lo podes encontrar en CoffeeBahia</h3>
+    <h3></h3>
+  </b-container>
     </div>
 </template>
 
